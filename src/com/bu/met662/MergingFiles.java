b63a6e2cceca2f4cba8caa01843d8fb752d52788
mannpro-05 <mannprajapati567@gmail.com>
2021-09-26 12:49:35 -0400

    Comments in MergeFiles.java

--- src/com/bu/met662/MergingFiles.java
+++ src/com/bu/met662/MergingFiles.java
@@ -3,7 +3,8 @@ package com.bu.met662;
 import constants.Constants;
 
 import java.io.*;
-
+/**
+ * This class Merges all the csv files in the zipFIle folder.*/
 public class MergingFiles {
   private final File f = new File(Constants.BASEPATH);
   private final String[] pathNames = f.list();
@@ -11,6 +12,10 @@ public class MergingFiles {
 
   public MergingFiles() throws IOException {}
 
+  /**
+   * This method iterates through all the folder and files.
+   * Reads every file line by line.
+   * Append every line into the merge.csv file. */
   void mergeFiles() throws IOException {
     final FileWriter csvWriter = new FileWriter(Constants.MERGED_CSV_FILE_NAME);
     if (pathNames != null) {
